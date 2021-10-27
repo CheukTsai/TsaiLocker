b7c87429a2612289b901823134e77249e162afbe
zhuocaili <li.zhuoca@northeastern.edu>
2021-10-27 13:50:36 -0700

    set up refs for login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 class Login extends React.Component {
 
+  emailRef = React.createRef()
+  passwordRef = React.createRef()
+
   handleSubmit = event => {
     //prevent default action
     event.preventDefault();
 
-    //
+    //acquire form data
+    console.log(this.emailRef.current.value)
+    const formData = {
+      email: this.emailRef.current.value,
+      password: this.passwordRef.current.value
+    };
+    console.log(formData)
+
+    //switch to home page
     this.props.history.push('/')
   }
 
@@ -18,13 +29,13 @@ class Login extends React.Component {
           <div className="field">
             <label className="label">Email</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Email" />
+              <input className="input" type="text" placeholder="Email" ref={this.emailRef} />
             </div>
           </div>
           <div className="field">
             <label className="label">Password</label>
             <div className="control">
-              <input className="input" type="password" placeholder="Password" />
+              <input className="input" type="password" placeholder="Password" ref={this.passwordRef} />
             </div>
           </div>
           <div className="control">
