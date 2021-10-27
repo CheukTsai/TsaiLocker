57bf90829928ba44c8a76c62d2292bb9a27bb6df
zhuocaili <li.zhuoca@northeastern.edu>
2021-10-27 15:07:36 -0700

    adding products

---
+++ src/components/Product.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { formatPrice } from 'commons/helper';
+
+class Product extends React.Component {
+
+    render() {
+        const { name, image, tags, price } = this.props.product;
+        return (
+            <div className="product">
+                <div className="p-content">
+                    <figure className="image is-4by3">
+                        <img src={image} alt={name} />
+                    </figure>
+                    <p className="p-tags">{tags}</p>
+                    <p className="p-name">{name}</p>
+                </div>
+                <div className="p-footer">
+                    <p className="price">{formatPrice(price)}</p>
+                    <button className="add-cart">
+                        <i className="fas fa-shopping-cart"></i>
+                        <i className="fas fa-exclamation"></i>
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Product;
